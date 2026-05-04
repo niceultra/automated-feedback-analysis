db35460b202200dd9d29655e674da910fe481a59
yredka <yredka/2003@mail.ru>
2026-05-04 17:45:53 +0800

    Redesign

--- web_app/app.py
+++ web_app/app.py
@@ -22,7 +22,7 @@ local_css("style.css")
 
 
 # 1. Открываем изображение с помощью PIL
-img = Image.open("images/logo.png")
+img = Image.open("./images/logo.png")
 
 # 2. Передаем объект изображения в конфигурацию
 st.set_page_config(
